cf072a70a273b6c892c89349ac42ee6803a3039e
JannisNe <necker@physik.hu-berlin.de>
2021-04-03 21:16:12 +0200

    adjust test

--- muvi_maker/core/pictures/__init__.py
+++ muvi_maker/core/pictures/__init__.py
@@ -1,6 +1,7 @@
 from .base_picture import BasePicture, PictureError
 from .star import Star
+from .circle import Circle
 from .add_bounce import *
 from .background import Background
 from .meta_circle import MetaCircle
-from .circle import Circle
+
