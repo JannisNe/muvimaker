2230713e97559293e03fbc7f2d47319b9b19b633
JannisNe <necker@physik.hu-berlin.de>
2021-04-03 23:48:16 +0200

    remove editor test, add PH test

--- test/test_editor.py
+++ test/test_editor.py
@@ -11,17 +11,17 @@ logger = main_logger.getChild(__name__)
 class TestEditor(unittest.TestCase):
     """Tests the setup of the GUI"""
 
-    def test_set_up(self):
-        logger.info('testing set up of GUI')
-        scratch = os.environ.get(mv_scratch_key, None)
-
-        if (not scratch) or (not os.path.isdir(scratch)):
-            home = os.path.expanduser('~')
-            logger.debug(f'{scratch} was given as scratch! Setting to {home}')
-            os.environ[mv_scratch_key] = home
-
-        root = tk.Tk(className='MuviMaker')
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
-        root.geometry(f'{round(screen_width / 2)}x{round(screen_height / 2)}')
-        Editor(root)
+    # def test_set_up(self):
+    #     logger.info('testing set up of GUI')
+    #     scratch = os.environ.get(mv_scratch_key, None)
+    #
+    #     if (not scratch) or (not os.path.isdir(scratch)):
+    #         home = os.path.expanduser('~')
+    #         logger.debug(f'{scratch} was given as scratch! Setting to {home}')
+    #         os.environ[mv_scratch_key] = home
+    #
+    #     root = tk.Tk(className='MuviMaker')
+    #     screen_width = root.winfo_screenwidth()
+    #     screen_height = root.winfo_screenheight()
+    #     root.geometry(f'{round(screen_width / 2)}x{round(screen_height / 2)}')
+    #     Editor(root)
---
+++ test/test_project_handler.py
@@ -0,0 +1,44 @@
+import unittest, os
+
+from muvi_maker import main_logger, mv_scratch_key
+from muvi_maker.core.project import ProjectHandler, standard_screen_size, standard_framerate, standard_hop_length
+from muvi_maker.example_data import example_song
+from muvi_maker.core.pictures.base_picture import BasePicture
+
+
+logger = main_logger.getChild(__name__)
+
+sounds = {'main': example_song}
+
+pictures = dict()
+pictures['bg'] = {
+    'background',
+    ['colour: 0'],
+    '0'
+}
+
+for i, pic_class in enumerate(BasePicture.subclasses()):
+
+    pictures[f'{i}'] = {
+        pic_class,
+        ['colour: main', 'radius: main'],
+        f'{i}'
+    }
+
+filename = os.path.join(os.environ[mv_scratch_key], 'test_ph_handler.pkl')
+
+
+class TestProjectHandler(unittest.TestCase):
+
+    def test_project_handler(self):
+
+        ph = ProjectHandler.get_project_handler(filename=filename)
+        ph.sound_files = sounds
+        ph.pictures = pictures
+        ph._main_sound_file = example_song
+        video = ph.get_video(
+            standard_screen_size, 
+            standard_hop_length,
+            standard_framerate
+        )
+        video.make_frame_per_frame(0)
