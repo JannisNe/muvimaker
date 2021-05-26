cef7489e0bd20250448a627984496d398603d2dc
JannisNe <necker@physik.hu-berlin.de>
2021-05-27 00:41:52 +0200

    add meta_rectangle

--- muvi_maker/core/pictures/__init__.py
+++ muvi_maker/core/pictures/__init__.py
@@ -3,6 +3,7 @@ from .star import Star
 from .circle import Circle
 from .background import Background
 from .meta_circle import MetaCircle
+from .meta_rectangle import MetaRectangle
 from .star_with_bounce import StarWithBounce
 from .circle_with_bounce import CircleWithBounce
 from .pre_computed_picture import PreComputedPicture
