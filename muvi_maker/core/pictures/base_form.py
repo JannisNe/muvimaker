951508c657b3cb22c2738998fc5e0465e552bffa
JannisNe <necker@physik.hu-berlin.de>
2021-05-26 09:59:24 +0200

    add saturation and sensitivity threshold for radius

--- muvi_maker/core/pictures/base_form.py
+++ muvi_maker/core/pictures/base_form.py
@@ -40,17 +40,32 @@ class BaseForm(BasePicture, abc.ABC):
         radius_sound_name = param_info['radius']
         max_radius = float(param_info.get('max_radius',  0.5)) * min(self.screen_size)
         radius_smooth = int(param_info.get('radius_smooth', 1))
+        radius_saturation_threshold = float(param_info.get('radius_saturation_threshold', 1))
+        radius_sensitive_threshold = float(param_info.get('radius_sensitive_threshold', 0))
 
         radius_sound = self.sound_dict[radius_sound_name]
-        radius_power = radius_sound.get_power()
-        radius = radius_power / max(radius_power) * max_radius
+        radius = radius_sound.get_power()
 
+        # if smooth is given select the maximum radius value
+        # from the given number of frames
         if radius_smooth > 1:
             rl = list()
             for i in range(len(radius)):
                 h, b = np.histogram(radius[i:i + radius_smooth])
                 rl.append(b[np.argmax(h)])
             radius = np.array(rl)
+
+        # all values below the sensitive threshold will be zero
+        radius_sensitive_mask = radius <= max(radius) * radius_sensitive_threshold
+        radius[radius_sensitive_mask] = 0.
+
+        # all values above the saturation threshold will be equal to the maximum
+        # of the values below this threshold
+        radius_saturation_mask = radius >= max(radius) * radius_saturation_threshold
+        radius[radius_saturation_mask] = max(radius[~radius_saturation_mask])
+
+        # scale so the maximum of radius is the given value
+        radius = radius / max(radius) * max_radius
 
         self.radius = radius
 
