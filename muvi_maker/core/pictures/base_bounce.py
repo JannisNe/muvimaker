07be7f3930590bf7835b1d50b70e7b98b53e564d
JannisNe <necker@physik.hu-berlin.de>
2021-06-07 09:20:22 +0200

    add bounce_radius_sensitive_radius

--- muvi_maker/core/pictures/base_bounce.py
+++ muvi_maker/core/pictures/base_bounce.py
@@ -17,6 +17,7 @@ class BaseBounce(BasePicture, abc.ABC):
         self.max_stroke_width = float(param_info.pop('max_stroke_width', 8))
         self.min_stroke_width = float(param_info.pop('min_stroke_width', 1))
         self.stroke_width_decline_order = float(param_info.pop('stroke_width_decline_order', 2))
+        self.bounce_sensitive_radius = float(param_info.pop('bounce_radius_sensitive_radius', 0.))
 
         self._b = (self.min_stroke_width - self.max_stroke_width * self.multiplicity ** self.stroke_width_decline_order) / \
                   (1 - self.multiplicity ** self.stroke_width_decline_order)
