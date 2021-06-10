cdda0bd679afbd91584b836f885d7ecad85972ac
JannisNe <necker@physik.hu-berlin.de>
2021-06-10 17:36:59 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 if __name__ == '__main__':
     setuptools.setup(
         name="muvimaker",
-        version="0.2.6-beta",
+        version="0.2.7",
         author="Jannis Necker",
         author_email="jannis.necker@gmail.com",
         description="A small package to generate moving pictures from sound",
