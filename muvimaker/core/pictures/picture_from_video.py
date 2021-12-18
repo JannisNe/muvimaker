86ddc41f09e3ef75152862ad90cea7bafb9339d1
JannisNe <necker@physik.hu-berlin.de>
2021-12-18 21:57:05 +0100

    add warp

--- muvimaker/core/pictures/picture_from_video.py
+++ muvimaker/core/pictures/picture_from_video.py
@@ -1,5 +1,5 @@
 from moviepy.editor import VideoFileClip
-import PIL, collections
+import PIL, collections, pickle, json
 import numpy as np
 from skimage import transform
 
@@ -14,10 +14,17 @@ logger = main_logger.getChild(__name__)
 @BasePicture.register_subclass('picture_from_video')
 class PictureFromVideo(SimplePicture):
 
+    needs_face_reco_cache = True
+
     def __init__(self, sound_dictionary, param_info, screen_size):
         filename = param_info['video_file']
 
         self.loop = bool(param_info.pop('loop', 'true'))
+
+        # ------------------------- Video --------------------------- #
+        self.video = VideoFileClip(filename, audio=False)
+        self.fps = self.video.fps
+        self._N_frames = int(round(self.video.duration * self.fps))
 
         # --------------------- Position ----------------------- #
         rel_center = param_info.get('center', '1, 1')
@@ -38,9 +45,74 @@ class PictureFromVideo(SimplePicture):
 
         self.center = np.array(screen_size) * rel_center
 
-        self.video = VideoFileClip(filename, audio=False)
-        self.fps = self.video.fps
-        self._N_frames = int(round(self.video.duration * self.fps))
+        # ------------------------ Warp ----------------------------- #
+        self.do_bubble_warp = bool(param_info.get('bubble_warp', False))
+        self.bubble_warp_mode = None
+
+        if self.do_bubble_warp:
+            self.bubble_warp_mode = param_info.get('bubble_warp_mode', 'static')
+
+            if self.bubble_warp_mode == 'static':
+                rel_bubble_warp_center = param_info.get('bubble_warp_center', '1, 1')
+                self.bubble_warp_center = np.array([float(i) for i in rel_bubble_warp_center.split(', ')] *
+                                                   self._N_frames) * np.array(screen_size)
+
+            if self.bubble_warp_mode == 'face_reco':
+                facereco_cache_file = param_info['face_reco_cache_file']
+                with open(facereco_cache_file, 'rb') as f:
+                    facereco_cache = pickle.load(f)
+
+                video_nickname = param_info['bubble_warp_face_reco_video_nickname']
+
+                marks = list(facereco_cache[video_nickname][0].keys())
+                face_map = json.loads(param_info['bubble_warp_face_map'])
+
+                radii = list()
+                centers = list()
+                for face_name, face_inds in face_map.items():
+
+                    given_params = list()
+                    for p, v in param_info.items():
+                        if ('bubble_warp' in p) and (f'radius_{face_name}' in p):
+                            given_params.append(p)
+
+                    landmark_specific = False
+                    for m in marks:
+                        for p in given_params:
+                            landmark_specific = landmark_specific or (m in p)
+
+                    update_param = dict()
+                    new_given_params = list()
+                    if not landmark_specific:
+                        for m in marks:
+                            for p in given_params:
+                                landmark_p = p.replace(f'radius_{face_name}', f'radius_{face_name}_{m}')
+                                if landmark_p == p:
+                                    continue
+                                update_param[landmark_p] = param_info[p]
+                                new_given_params.append(landmark_p)
+
+                        given_params = new_given_params
+
+                    param_info.update(update_param)
+
+                    update_param = dict()
+                    for face_ind in face_inds:
+                        for m in marks:
+                            r = f'radius_{face_ind}_{m}'
+                            radii.append(f'bubble_warp_{r}')
+                            centers.append(facereco_cache[video_nickname][face_ind][m])
+                            for p in given_params:
+                                ind_param = p.replace(f'radius_{face_name}_{m}', r)
+                                if p == ind_param:
+                                    continue
+                                param_info[ind_param] = param_info[p]
+
+                    param_info.update(update_param)
+
+                self.bubble_warp_center = np.array(centers) * np.array(screen_size)
+                param_info['radii'] = radii
+
         super(PictureFromVideo, self).__init__(sound_dictionary, param_info, screen_size)
 
     def t(self, ind):
@@ -49,7 +121,7 @@ class PictureFromVideo(SimplePicture):
     def _make_frame_per_frame(self, ind):
         frame = self.video.get_frame(self.t(ind % self._N_frames))
         image = PIL.Image.fromarray(frame)
-        image = image.resize(self.screen_size.astype(int)*2, PIL.Image.NEAREST)
+        image = image.resize(np.array(self.screen_size).astype(int)*2, PIL.Image.NEAREST)
         return np.array(self.postprocess(image, ind))
 
     @staticmethod
@@ -75,15 +147,17 @@ class PictureFromVideo(SimplePicture):
         direction = bmask / dmask
         new = dmask / (dmask - dminusrmask) * dminusrmask * direction
         a[mask_r] += np.sum(new, axis=0, where=mask[:,mask_r, None])
-
         return a.astype(int)
 
     def postprocess(self, frame, ind):
         frame = super().postprocess(frame, ind)
-        centers = self.center if self._static_center else self.center[ind % self._N_frames]
-        radii = np.array([self.__getattribute__(r)[ind] for r in self.radii])
-        args = {'centers': np.atleast_1d(centers),
-                'radius': radii}
-        frame = transform.warp(image=np.array(frame), inverse_map=self._bubble_warp, map_args=args)
-        frame = (frame * 255).astype(np.uint8)
+
+        if self.do_bubble_warp:
+            # centers = self.center if self._static_center else self.center[ind % self._N_frames]
+            centers = self.bubble_warp_center[:, ind % self._N_frames]
+            radii = np.array([self.__getattribute__(r)[ind] for r in self.radii if hasattr(self, r)])
+            args = {'centers': np.atleast_1d(centers),
+                    'radius': radii}
+            frame = transform.warp(image=np.array(frame), inverse_map=self._bubble_warp, map_args=args)
+            frame = (frame * 255).astype(np.uint8)
         return frame
